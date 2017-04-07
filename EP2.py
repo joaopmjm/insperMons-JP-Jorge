85b681df83ecd60ded14a036c62f7a26fb9311c5
joaopmjm <joaopmjm@al.insper.edu.br>
2017-04-07 16:37:01 -0300

    1.1
    melhoria nas listas e inicio do jogo

--- EP2.py
+++
@@ -1,7 +0,0 @@
-ipmons={"fransauro","toranxu","peterodáctil","lecachop","annatchu","jorgenite"}
-attck={"fransauro":50,"toranxu":10,"peterodáctil":20,"lecachop":60,"annatchu":35,"jorgenite":100}
-defesa={"fransauro":150,"toranxu":75,"peterodáctil":500,"lecachop":125,"annatchu":300,"jorgenite":250}
-vida={"fransauro":500,"toranxu":300,"peterodáctil":1000,"lecachop":650,"annatchu":600,"jorgenite":750}
-def batalha(ipmons,attck,defesa):
-	while vida[ipmons]>0:
-		vida[ipmons]=vida[ipmons]-(attck[random]
---
+++ jogo.py
@@ -0,0 +1,7 @@
+while True:
+	p=input("Você quer: Passear ou Dormir")
+	if p=="Passear":
+		import ipmons.py
+		i=random.randit(ipmons)
+	if p=="Dormir":
+		continue
