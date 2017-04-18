d8d5d2e0a42d487d5597f56201938a1127bb3f8d
ehrhardt98 <jorgeavbe@al.insper.edu.br>
2017-04-18 16:39:57 -0300

    Add files via upload

--- Inspermon.py
+++ Inspermon.py
@@ -1,21 +1,35 @@
 import time
 import random
-Starters = {"0":["Atk","Def","Hp"],"1":["Atk","Def","Hp"],"2":["Atk","Def","Hp"],"3":["Atk","Def","Hp"],}
-Starter = input("Escolha um dos três Inspèrmons iniciais: 1,2,3\n")
 
-dex = ["0"={"Atk":10,"Def":2,"Hp":150},"1"={"Atk":15,"Def":1,"Hp":200},"2"={"Atk":13,"Def","Hp"},"3"={"Atk","Def","Hp"},"4"={"Atk","Def","Hp"},"5"={"Atk","Def","Hp"},"6"={"Atk","Def","Hp"},"7"={"Atk","Def","Hp"},"8"={"Atk","Def","Hp"},"9"={"Atk","Def","Hp"},"10"={"Atk","Def","Hp"}]
-mons = ["1","2","3","4","5","6","7","8","9"]
+dex = {"0":["Atk","Def","Hp"],"1":["Atk","Def","Hp"],"2":["Atk","Def","Hp"],"3":["Atk","Def","Hp"],"4":["Atk","Def","Hp"],"5":["Atk","Def","Hp"],"6":["Atk","Def","Hp"],"7":["Atk","Def","Hp"],"8":["Atk","Def","Hp"],"9":["Atk","Def","Hp"],"10":["Atk","Def","Hp"]}
+mons = ["0","1","2","3","4","5","6","7","8","9"]
+Starters = ["0","1","2","3"]
+Starter=""
+while Starter != "0" and Starter !="1" and Starter !="2" and Starter !="3":
+	Starter = input("Escolha um dos três Inspèrmons iniciais: 1,2,3\n")
+	if Starter == "0":
+		print("Você descobriu o Inspermon secreto! O {} agora é seu!")
+	elif Starter == "1" or Starter == "2" or Starter == "3":
+		print("Você escolheu o {}".format(Starter))
+	else:
+		print("Infelizmente, {} não é uma escolha válida... tente novamente.".format(Starter))
+Escolha_stats = dex[Starter]
+
+
 
 def rand():
 	randomnum = random.randint(0,9)
 	return randomnum
+
+
 while True:
-	Escolha=input("Escolha seu pokemon")
-	i = input("Vamos batalhar? Ou vamos dormir?\n")
+
+	i = input("Você quer ir batalhar ou dormir?  b/d\n")
 
 	if i == "d":
 		print("Nos vemos na próxima!")
 		break
+
 
 	if i == "b":
 		Oponente = mons[rand()]
@@ -26,7 +40,11 @@ while True:
 		while Escolha[2] > 0 and dex[Oponente][2] > 0: #Trocar os "1", "2"... pelos nomes s/ ""
 			n = input("Atacar ou fugir? a/f")
 			if n == "f":
-				print("corre negadaaaaaaaa")
-				break
+				print("calma fião")
+				continue
 			if n == "a":
-				print("{} eu escolho você".format(Escolha))
+				1==1
+
+
+	else:
+		print("Essa não é uma escolha disponível... tente novamente")
