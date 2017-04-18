314becb52a632d28d0bbb6b41454bb2491d367d8
joaopmjm <joaopmjm@al.insper.edu.br>
2017-04-18 19:06:18 -0300

    ai ai
    anhaha

--- Inspermon 2.0.py
+++ Inspermon 2.0.py
@@ -1,21 +1,19 @@
 import time
 import random
 import sys
-dex = {"0":["Atk","Def","Hp"],"1":["Atk","Def","Hp"],"2":["Atk","Def","Hp"],"3":["Atk","Def","Hp"],"4":["Atk","Def","Hp"],"5":["Atk","Def","Hp"],"6":["Atk","Def","Hp"],"7":["Atk","Def","Hp"],"8":["Atk","Def","Hp"],"9":["Atk","Def","Hp"],"10":["Atk","Def","Hp"]}
-mons = ["Narf","Jackal","Ash","Canbo","Toranxu","Blizz","Karkan","Nicks","Chanka","Bumtu"]
+dex = {"Narf":[20,30,60],"Jomander":[50,30,40],"Squirtle":[30,50,40],"Bulbassauro":[40,50,30],"Pikachu":[60,50,40],"Hage-Monstro":[50,50,70],"Hitmon-Jorge":[55,25,40],"Toranxu":[35,15,60],"Alex":[35,15,60],"Gokuma":[35,15,60],"Batatrampo":[35,15,60]}
+mons = ["Narf","Jomander","Squirtle","Bulbassauro","Toranxu","Pikachu","Hage-Monstro","Hitmon-Jorge","Alex","Gokuma","Batatrampo"]
 Starters = {"Squirtle":[30,50,40],"Jomander":[50,30,40],"Bulbassauro":[40,50,30],"Pikachu":[60,50,40]}
 Starter=""
 while Starter != "Squirtle" and Starter !="Jomander" and Starter !="Bulbassauro" and Starter !="Pikachu":
-	Starter = input("Escolha um dos três Inspèrmons iniciais:\n Squirtle {} \n,Jomander {} \n Bulbassauro {} \n".format(Starters["Squirtle"],Starters["Jomander"],Starters["Bulbassauro"]))
+	Starter = input("Escolha um dos três Inspèrmons iniciais:\n Squirtle {} \n Jomander {} \n Bulbassauro {} \n".format(Starters["Squirtle"],Starters["Jomander"],Starters["Bulbassauro"]))
 	if Starter == "Pikachu":
 		print("Você descobriu o Inspermon secreto! O Pikachu {} agora é seu!".format(Starters["Pikachu"]))
 	elif Starter == "Squirtle" or Starter == "Jomander" or Starter == "Bulbassauro":
-		print("Você escolheu o {} \n ataque: {} \n defesa: {} \n Vida: {}".format(Starter, Starters["Starter"[0]],Starters["Starter"[1]],Starters["Starter"[2]]))
+		print("Você escolheu o {} \n ataque: {} \n defesa: {} \n Vida: {}".format(Starter, Starters[Starter][0],Starters[Starter][1],Starters[Starter][2]))
 	else:
 		print("Infelizmente, {} não é uma escolha válida... tente novamente.".format(Starter))
 Escolha_stats = dex[Starter]
-
-
 
 def rand():
 	randomnum = random.randint(0,9)
@@ -44,12 +42,12 @@ while True:
 		time.sleep(rand())
 		print("A wild {} aparece!".format(Oponente))
 
-		while Escolha[2] > 0 and dex[Oponente][2] > 0: #Trocar os "1", "2"... pelos nomes s/ ""
+		while Escolha_stats[2] > 0 and dex[Oponente][2] > 0: #Trocar os "1", "2"... pelos nomes s/ ""
 			n = input("Atacar ou fugir? a/f")
 			if n == "f":
 				print("calma fião")
 				continue
 			if n == "a":
-				batalha(Starter,Oponente)															
+				batalha(Starter,Oponente)														
 	else:
 		print("Essa não é uma escolha disponível... tente novamente")
