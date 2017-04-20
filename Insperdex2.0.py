f1bc040d2b70a5df6d96f1f893875baf0f0ae892
ehrhardt98 <jorgeavbe@al.insper.edu.br>
2017-04-20 09:11:05 -0300

    Add files via upload

---
+++ Insperdex2.0.py
@@ -0,0 +1,9 @@
+def insperdex(Oponente):
+	Inspdex = []
+	Inspdex.append(Oponente)
+	Insperdex = list(set(Inspdex))
+	return Insperdex
+
+
+
+Insp = Insperdex(Oponente) #Colocar dentro do lop da batalha
