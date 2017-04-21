700b9af9ab397e510d45fd053d7ee8c759358655
joaopmjm <joaopmjm@al.insper.edu.br>
2017-04-21 20:59:27 -0300

    3.1
    melhorando

--- Inspermon3.0.py
+++ Inspermon3.0.py
@@ -1,18 +1,18 @@
 from time import sleep
 from random import choice, randint
 from os import system, name
-
+system('cls' if name == 'nt' else 'clear')
 # Modelo: 'Nome': [Ataque, Defesa, Vida]
-dex = {'Narf': [20, 30, 60], 'Jomander': [50, 30, 40],
-       'Squirtle': [30, 50, 40], 'Bulbassauro': [40, 50, 30],
-       'Pikachu': [60, 50, 40], 'Hage-Monstro': [50, 50, 70],
-       'Hitmon-Jorge': [55, 25, 40], 'Toranxu': [35, 15, 60],
-       'Alex': [35, 15, 60], 'Gokuma': [35, 15, 60],
-       'Batatrampo': [35, 15, 60]}
+dex = {'Narf': [200, 3, 600], 'Jomander': [500, 3, 400],
+       'Squirtle': [300, 5, 400], 'Bulbassauro': [400, 5, 300],
+       'Pikachu': [600, 5, 400], 'Hage-Monstro': [500, 5, 700],
+       'Hitmon-Jorge': [550, 2.5, 400], 'Toranxu': [350, 1.5, 600],
+       'Alex': [350, 1.5, 600], 'Gokuma': [350, 1.5, 600],
+       'Batatrampo': [350, 1.5, 600]}
 
 mons = dex.keys()
-starters = {'Squirtle': [30, 50, 40], 'Jomander': [50, 30, 40],
-            'Bulbassauro': [40, 50, 30], 'Pikachu': [60, 50, 40]}
+starters = {'Squirtle': [300, 5, 400], 'Jomander': [500, 3, 400],
+            'Bulbassauro': [400, 3, 300], 'Pikachu': [600, 5, 400]}
 
 starter = ''
 while starter not in starters.keys():
@@ -22,10 +22,11 @@ while starter not in starters.keys():
     Bulbassauro {} \n'''.format(starters['Squirtle'],
                                 starters['Jomander'],
                                 starters['Bulbassauro']))
+    starter=starter.title()
     if starter == 'Pikachu':
         system('cls' if name == 'nt' else 'clear')
         print('Você descobriu o Inspermon secreto!')
-        print('O Pikachu {} agora é seu!'.format(starters['Pikachu']))
+        print('O Pikachu {} agora é seu! \n \n \n aperte "enter" para continuar'.format(starters['Pikachu']))
 
     elif starter == 'Squirtle' or \
            starter == 'Jomander' or \
@@ -34,7 +35,7 @@ while starter not in starters.keys():
         print('''Você escolheu o {}
         ataque: {}
         defesa: {}
-        Vida: {}'''.format(starter,  starters[starter][0],
+        Vida: {} \n \n \n aperte "enter" para continuar'''.format(starter,  starters[starter][0],
                            starters[starter][1], starters[starter][2]))
     else:
         print('''Infelizmente, {} não é uma escolha válida...
@@ -72,42 +73,110 @@ def batalha(game_poke, oponente):
         print('\n\n\tVida {}: {}'.format(game_poke, seu[hp]))
         print('\n\n\tVida {}: {}\n\n'.format(oponente, adv[hp]))
 
-        adv[hp] = adv[hp] - (seu[atk] - adv[defe])
+        adv[hp] = adv[hp] - (seu[atk] / adv[defe])
 
         if adv[hp] <= 0:
             return '{} venceu a batalha!'.format(game_poke)
         if adv[hp] > 0:
-            seu[hp] = seu[hp] - (adv[atk] - seu[defe])
-            if seu[hp] <= 0:
-                return '{} perdeu!'.format(oponente)
-            if seu[hp] > 0:
-                pass
+        	choi=input("Atacar ou fugir? \n\n")
+        	choi=choi.title()
+        	if choi =="Atacar":
+	            seu[hp] = seu[hp] - (adv[atk] / seu[defe])
+	            if seu[hp] <= 0:
+	                return '{} perdeu!'.format(oponente)
+	            if seu[hp] > 0:
+	            	pass
+	       	if choi == "Fugir":
+	       		run=randint(0,9)
+	       		if run==0:
+	       			print("Você não conseguiu fugir! o adversário começa atacando")
+	       		if run!=0:
+	       			print("Você fugiu!")
+	       			break
+def batalhaperda(game_poke, oponente):
+    """
+    Função batalha.
+    Essa função executa uma batalha entre seu pokémon e um pokémon randômico
+    Args:
+        - game_poke: Seu pokémon
+        - oponente: Pokémon do oponente
+    Vars:
+        - Hp: Vida
+        - atk: Ataque
+        - defe: Defesa
+        - seu: Seu pokemon
+        - adv: Pokémon adversário
+    """
+    system('cls' if name == 'nt' else 'clear')
+    print('######### BATALHA #########')
+    print('######### {} Vs. {} #########'.format(game_poke, oponente))
+    hp = 2
+    atk = 0
+    defe = 1
+    seu = dex[game_poke]
+    adv = dex[oponente]
+    cont = 1
+    while True:
+        print('Rodada {}'.format(cont))
+        print('\n\n\tVida {}: {}'.format(game_poke, seu[hp]))
+        print('\n\n\tVida {}: {}\n\n'.format(oponente, adv[hp]))
+
+        seu[hp] = seu[hp] - (adv[atk] / seu[defe])
+
+        if seu[hp] <= 0:
+            return 'você perdeu'
+        if seu[hp] > 0:
+        	choi=input("Atacar ou fugir? \n\n")
+        	choi=choi.title()
+        	if choi =="Atacar":
+	            adv[hp] = adv[hp] - (seu[atk] / adv[defe])
+	            if adv[hp] <= 0:
+	                return '{} perdeu!'.format(oponente)
+	            if adv[hp] > 0:
+	            	pass
+	       	if choi == "Fugir":
+	       		run=randint(0,9)
+	       		if run==0:
+	       			print("Você não conseguiu fugir!")
+	       		if run!=0:
+	       			print("Você fugiu!")
+	       			break
         cont += 1
-
-
 while True:
     system('cls' if name == 'nt' else 'clear')
-    i = input('Você quer ir batalhar ou dormir? \n \n b ou d \n \n')
+    i = input('Você quer ir batalhar ou dormir? \n \n')
+    i=i.title()
 
-    if i == 'd':
+    if i == 'Dormir':
         print('Nos vemos na próxima!')
         break
 
-    if i == 'b':
+    if i == 'Batalhar':
         char = choice(list(mons))
         oponente = dex[char]
         print('Procurando por um oponente...')
-        sleep(randint(1, 10))
+        sleep(randint(1, 5))
         system('cls' if name == 'nt' else 'clear')
         print('A wild {} {} aparece!'.format(char, oponente))
 
         while escolha_stats[2] > 0 and oponente[2] > 0:
-            n = input('Atacar ou fugir? \n a/f \n \n')
-            if n == 'f':
-                print('calma fião')
-                continue
-            if n == 'a':
+            n = input('Atacar ou fugir? \n \n')
+            n=n.title()
+            if n == 'Fugir':
+                ran=randint(0,100)
+                if ran>70:
+                	print('Você não conseguiu fugir dessa batalha')
+                	sleep(3)
+                	print(batalhaperda(starter, char))
+                	sleep(300)
+                	break
+                if ran<=70:
+                	print('Você fugiu')
+                	sleep(3)
+                	break
+            if n == 'Atacar':
                 print(batalha(starter, char))
-                input()
+                sleep(3)
+                break
     else:
         print('Essa não é uma escolha disponível... tente novamente')
